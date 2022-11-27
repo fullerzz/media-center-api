6932615389218f7d4cd28f33c8d6e10417db6892
Zach Fuller <zach.fuller1222@gmail.com>
2022-11-27 13:14:47 -0700

    first commit

---
+++ src/main/java/com/fullerzz/mediacenter/MediaCenterApplication.java
@@ -0,0 +1,13 @@
+package com.fullerzz.mediacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullerzz/mediacenter/MediaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullerzz.mediacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
