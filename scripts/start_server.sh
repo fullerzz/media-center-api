a4848fdbc49f68bb408e6257b71d05ab5d47581e
Zach Fuller <zach.fuller1222@gmail.com>
2022-12-22 15:48:58 -0700

    Updated start script with maven command

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java -jar media-center-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+cd ../ && mvn verify
+touch temp.txt
+java -jar target/media-center-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
