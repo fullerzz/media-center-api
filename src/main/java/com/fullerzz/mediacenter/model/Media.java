9dab2c0ac357bf6aeb274b3b25333d47c3ad2412
Zach Fuller <zach.fuller1222@gmail.com>
2022-12-26 15:02:02 -0700

    Implemented single file downloads

--- src/main/java/com/fullerzz/mediacenter/model/Media.java
+++ src/main/java/com/fullerzz/mediacenter/model/Media.java
@@ -2,7 +2,5 @@ package com.fullerzz.mediacenter.model;
 
 public interface Media {
     String getFilename();
-    int getFileSize();
-    void setFilename(String filename);
-    void setFileSize(int fileSize);
+    long getFileSize();
 }
